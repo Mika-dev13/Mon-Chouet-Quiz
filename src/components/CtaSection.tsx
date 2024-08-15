668c09937f3f42bdb486bf51aebeb880d3affe9f
Mika_dev <michealchorro.dev@gmail.com>
2024-08-15 12:37:40 +0200

    Refactor CtaSection and Footer components

--- src/components/CtaSection.tsx
+++ src/components/CtaSection.tsx
@@ -4,21 +4,26 @@ import foret from '../../public/foret.webp';
 
 const CtaSection = () => {
   return (
-    <section className='rounded-md relative overflow-hidden grid grid-cols-2 gap-4 bg-teal-100'>
-      <Image
-        src={foret}
-        alt='forêt'
-        className='rounded-md w-full lg:min-h-88 object-cover'
-      />
-      <div className='flex flex-col items-center justify-center px-8'>
-        <h2 className='text-xl mb-8 font-semibold text-center'>
-          Inscris-toi{' '}
-          <span className='text-rose-500 font-bold'>gratuitement</span> sans
-          plus tarder petit aventurier et pars à la découverte de nouvelles
-          connaissances !
-        </h2>
+    <section className=''>
+      <h2 className='text-center lg:text-2xl font-semibold mb-16'>
+        Prêt pour l&apos;aventure ?{' '}
+      </h2>
+      <div className='grid grid-cols-2 gap-4 bg-cyan-100 rounded-md overflow-hidden'>
+        <Image
+          src={foret}
+          alt='forêt'
+          className='w-full lg:max-h-96 object-cover'
+        />
+        <div className='flex flex-col items-center justify-center px-9'>
+          <p className='text-xl mb-8 text-center'>
+            Inscris-toi{' '}
+            <span className='text-rose-500 font-bold'>gratuitement</span> sans
+            plus tarder petit aventurier et pars à la découverte de nouvelles
+            connaissances !
+          </p>
 
-        <Button className='uppercase'>à l&apos;aventure !</Button>
+          <Button className='uppercase'>à l&apos;aventure !</Button>
+        </div>
       </div>
     </section>
   );
